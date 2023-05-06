4210f5cf346ba248d7290d183247de0078d74e26
alyssaw198 <alyssawu0104@gmail.com>
2023-05-06 00:17:16 -0400

    Configuring HTML

--- django/MetHacks 2023/methacksproject/main/urls.py
+++ django/MetHacks 2023/methacksproject/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from . import views
+from main import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home') #when there is no path specified, run the home function in views.py 
+
     #TODO: define the different paths (different web 'pages') associated with the website
 ]
