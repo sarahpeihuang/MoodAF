346e1a265608762b915974dad3598f47fb0398e8
alyssaw198 <alyssawu0104@gmail.com>
2023-05-06 01:01:11 -0400

    Just testing things

--- django/MetHacks 2023/methacksproject/main/urls.py
+++ django/MetHacks 2023/methacksproject/main/urls.py
@@ -5,6 +5,5 @@ from main import views
 
 urlpatterns = [
     path('', views.home, name='home') #when there is no path specified, run the home function in views.py 
-
     #TODO: define the different paths (different web 'pages') associated with the website
 ]
