f8ee55bcc8e89375846d1d62b95f692500961dc6
sarah888 <shuan392@uwo.ca>
2023-05-06 11:51:19 -0400

    Form edits

--- django/MetHacks 2023/methacksproject/main/urls.py
+++ django/MetHacks 2023/methacksproject/main/urls.py
@@ -5,6 +5,8 @@ from main import views
 
 urlpatterns = [
     path('', views.home, name='home'), #when there is no path specified, run the home function in views.py 
-    path('form', views.form, name='form')
+    path('form', views.form, name='form'),
+    path('postForm', views.postForm, name='postForm'),
+    path('summary', views.summary, name='summary')
     #TODO: define the different paths (different web 'pages') associated with the website
 ]
